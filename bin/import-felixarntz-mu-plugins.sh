ec691564b775352e3de7231a800bb9bdf368da79
Alex Kirk <akirk@users.noreply.github.com>
2024-07-12 09:17:13 +0200

    Update zip

--- bin/import-felixarntz-mu-plugins.sh
+++ bin/import-felixarntz-mu-plugins.sh
@@ -1,5 +1,6 @@
 cd `dirname $0`
 pushd ../felixarntz-mu-plugins/
+rm -f ../felixarntz-mu-plugins-shared.zip
 zip ../felixarntz-mu-plugins-shared.zip felixarntz-mu-plugins/shared/*
 popd
 node convert-felixarntz-mu-plugins.js
