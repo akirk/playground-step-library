89201eaf690b6f4175b135409fa5206078f5feb8
Alex Kirk <akirk@users.noreply.github.com>
2024-07-12 10:21:52 +0200

    Remove the plugins again, we'll only use them as inspiration

--- bin/import-felixarntz-mu-plugins.sh
+++
@@ -1,7 +0,0 @@
-cd `dirname $0`
-pushd ../felixarntz-mu-plugins/
-rm -f ../felixarntz-mu-plugins-shared.zip
-zip ../felixarntz-mu-plugins-shared.zip felixarntz-mu-plugins/shared/*
-popd
-node convert-felixarntz-mu-plugins.js
-node update-load-steps.js
