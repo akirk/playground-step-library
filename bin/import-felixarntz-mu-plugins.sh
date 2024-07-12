d8cc46c1ca701b886c56e133673a8b5475c54b9c
Alex Kirk <akirk@users.noreply.github.com>
2024-07-12 08:51:44 +0200

    Update paths inside the zip

--- bin/import-felixarntz-mu-plugins.sh
+++ bin/import-felixarntz-mu-plugins.sh
@@ -1,6 +1,6 @@
 cd `dirname $0`
-pushd ../felixarntz-mu-plugins/felixarntz-mu-plugins/shared
-zip ../../../felixarntz-mu-plugins-shared.zip *
+pushd ../felixarntz-mu-plugins/
+zip ../../../felixarntz-mu-plugins-shared.zip felixarntz-mu-plugins/shared/*
 popd
 node convert-felixarntz-mu-plugins.js
 node update-load-steps.js
