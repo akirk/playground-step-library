2d6bfc3191d1ac3cd0175f3de6885ebd2eb6fbd1
Alex Kirk <akirk@users.noreply.github.com>
2024-09-20 04:22:36 +0200

    Add phEditor and phpLiteAdmin

---
+++ steps/phEditor.js
@@ -0,0 +1,33 @@
+customSteps.phEditor = function( step ) {
+	const steps = [
+		{
+			"step": "mkdir",
+			"path": "/wordpress/wp-content/mu-plugins",
+		},
+		{
+			"step": "writeFile",
+			"path": "/wordpress/wp-content/mu-plugins/phEditor.php",
+			"data": `<?php
+add_action( 'admin_bar_menu', function( WP_Admin_Bar $wp_menu ) {
+        $wp_menu->add_node(
+                array(
+                        'id'     => 'pheditor',
+                        'title'  => 'phEditor',
+                        'href'   => '/pheditor-main/pheditor.php',
+                )
+        );
+}, 100 );`
+		},
+		{
+			"step": "unzip",
+			"zipFile": {
+				"resource": "url",
+				"url": "https://github-proxy.com/proxy/?repo=akirk/pheditor&branch=main",
+			},
+			"extractToPath": "/wordpress/"
+		}
+	];
+	steps.landingPage = "/pheditor-main/pheditor.php";
+	return steps;
+};
+customSteps.phEditor.info = "Install phEditor. Password: admin";
