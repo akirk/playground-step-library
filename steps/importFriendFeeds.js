a259c51f5bafca5622092870322d728615539b98
Alex Kirk <akirk@users.noreply.github.com>
2024-10-16 10:20:05 +0100

    Add an Import Friend Feeds step

---
+++ steps/importFriendFeeds.js
@@ -0,0 +1,28 @@
+customSteps.importFriendFeeds = function( step ) {
+	return [
+		{
+			"step": "runPHP",
+			"code": `
+<?php require_once 'wordpress/wp-load.php';
+if ( class_exists('Friends\Import')) {
+Friends\Import::opml(
+<<<OPML
+${step.vars.opml}
+OPML
+);
+do_action( 'cron_friends_refresh_feeds' );
+}
+	`
+		}
+	];
+};
+customSteps.importFriendFeeds.info = "Provide useful additional info.";
+customSteps.importFriendFeeds.vars = [
+	{
+		"name": "opml",
+		"description": "The OPML file",
+		"type": "textarea",
+		"required": true,
+		"samples": [ "" ]
+	}
+];
