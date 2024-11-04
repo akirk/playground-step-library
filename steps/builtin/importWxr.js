cae5b1349e97e141ec7a18c6a8f25206d8ebce7f
Alex Kirk <akirk@users.noreply.github.com>
2024-11-04 13:58:05 +0100

    Add a cors proxy option

--- steps/builtin/importWxr.js
+++ steps/builtin/importWxr.js
@@ -2,12 +2,13 @@ customSteps.importWxr = function( step ) {
 	if ( ! step.vars.url || ! step.vars.url.match( /^https?:/ ) ) {
 		return [];
 	}
+	const url = step.vars.corsProxy ? 'https://playground.wordpress.net/cors-proxy.php?' + step.vars.url : step.vars.url;
 	return [
 		{
 			"step": "importWxr",
 			"file": {
                 "resource": "url",
-                "url": step.vars.url
+                url
             }
 		}
 	];
@@ -20,5 +21,12 @@ customSteps.importWxr.vars = [
 		"description": "URL of a WXR file",
 		"required": true,
 		"samples": [ "" ]
+	},
+	{
+		"name": "corsProxy",
+		"description": "Use a cors proxy for the request",
+		"required": true,
+		"type": "boolean",
+		"samples": [ "true", "false" ]
 	}
 ];
