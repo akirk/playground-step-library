890651a8b151dd60f7ae05f357b84f5448c95a1c
Alex Kirk <akirk@users.noreply.github.com>
2024-12-18 15:03:45 +0100

    typo

--- steps/builtin/installPlugin.js
+++ steps/builtin/installPlugin.js
@@ -3,7 +3,7 @@ customSteps.installPlugin = function( step ) {
 	if ( urlTest ) {
 		return customSteps.githubPlugin( step );
 	}
-	let plugin = urlTest.groups.slug;
+	let plugin = steps.vars.url;
 	urlTest = /^(?:https:\/\/wordpress.org\/plugins\/)?(?<slug>[^\/]+)/.exec( step.vars.url );
 	if ( urlTest ) {
 		plugin = urlTest.groups.slug;
