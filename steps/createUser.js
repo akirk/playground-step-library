8b0efc5ef7598cb7fae94733a12f198f9e8d7e35
Alex Kirk <akirk@users.noreply.github.com>
2025-04-18 16:29:25 +0200

    Add a doAction step

--- steps/createUser.js
+++ steps/createUser.js
@@ -1,11 +1,11 @@
-customSteps.createUser = function( step ) {
+customSteps.createUser = function ( step ) {
 	let code = "<?php require_once 'wordpress/wp-load.php';";
 	let username = step?.vars?.username;
 	let password = step?.vars?.password;
 	let role = step?.vars?.role;
 	let display_name = step?.vars?.display_name;
 	let email = step?.vars?.email;
-	if ( ! username || ! password || ! role ) {
+	if ( !username || !password || !role ) {
 		return [];
 	}
 	code += ` $data = array( 'user_login' => '${username}', 'user_pass' => '${password}', 'role' => '${role}',`
@@ -20,8 +20,8 @@ customSteps.createUser = function( step ) {
 	code += "); wp_insert_user( $data ); ?>";
 	var steps = [
 		{
-            "step": "runPHP",
-            code
+			"step": "runPHP",
+			code
 		}
 	];
 	if ( step?.vars?.login ) {
@@ -52,9 +52,8 @@ customSteps.createUser.vars = [
 		"name": "role",
 		"description": "Role",
 		"required": true,
-		"type": "select",
-		"options": [ "administrator", "editor", "author", "contributor", "subscriber", "read", "client" ],
-		"samples": [ "editor" ]
+		"type": "text",
+		"samples": [ "administrator", "editor", "read", "client" ]
 	},
 	{
 		"name": "display_name",
