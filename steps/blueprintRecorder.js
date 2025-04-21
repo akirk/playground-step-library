ecfcc277cd35fdfe320ca71c7d0f16ba4513d91f
Alex Kirk <akirk@users.noreply.github.com>
2025-04-21 13:23:01 +0200

    Fix blueprint recorder

--- steps/blueprintRecorder.js
+++ steps/blueprintRecorder.js
@@ -1,9 +1,9 @@
-customSteps.blueprintRecorder = function( step ) {
+customSteps.blueprintRecorder = function ( step ) {
 	return customSteps.githubPlugin( {
 		vars: {
-			repo: "akirk/blueprint-recorder",
+			url: "https://github.com/akirk/blueprint-recorder",
 			branch: "main",
 		}
-	});
+	} );
 };
 customSteps.blueprintRecorder.info = "Record steps made and compile a new blueprint.";
